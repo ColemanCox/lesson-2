e8b5519c6357131452cb26b9b115f5af44295746
Coleman Cox <colezane82@gmail.com>
2015-09-01 12:06:28 -0500

    Updated double forward slash

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.get('/*', function(req, res) {
   var path = req.originalUrl;
 
-  request('https://api.forecast.io/forecast/c922d1154b4c3cb98e5d1e31d3b452c3/' + path).pipe(res);
+  request('https://api.forecast.io/forecast/c922d1154b4c3cb98e5d1e31d3b452c3' + path).pipe(res);
 });
 
 app.listen(process.env.PORT || 3000);
